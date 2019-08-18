906d7f63b881a01ae2f0dd3c42874066d50d77c1
Sahil Choudhary <sahil143c@gmail.com>
2019-08-18 18:57:23 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,20 +1,16 @@
-screen_name = "sahilpedazo"
+screen_name = "Your_Screen_Name"
 
-#visit https://apps.twitter.com/app/new for create new app on twitter developer account.
-#after create account you  need to generate  CONSUMER and ACCESS key token.
-# for more infomation  visit to my chennel 
-#https://www.youtube.com/channel/UCJh4EQMRPQf9Vw0ifMmv7bg/
+#visit https://apps.twitter.com/app/new to create new app on twitter developer account.
+#after you create the app, you  need to generate  CONSUMER and ACCESS key token.
 
-CONSUMER_KEY = "ybzSosLphDzvaQXXjr8OGoa38"
-CONSUMER_SECRET = "CHDrHKsD5AyKVb29v7qWaJ13dMWt5OvvvRw76zxSrtClNI13QP"
-ACCESS_TOKEN = "1395675786-DAFLJT76EObWDvXuaW80gMVCAf9Ld0N8x4us8jk"
-ACCESS_SECRET = "FOlEtHi2GpldYDRv9sWKdaizGGsimdwmSS7BA3sS2FKV7"
 
-results_search=20 #Result of keyword search for apply action.
-tweet_delete_days = 2
-blacklisted = ["sex","sexy","porn"] # keyword that you want to ignore
-# for blacklisted work in progress.
+CONSUMER_KEY = "Enter Consumer Key Here"
+CONSUMER_SECRET = "Enter Consumer Secret Here"
+ACCESS_TOKEN = "Enter Access Token Here"
+ACCESS_SECRET = "Enter Access Secret Here"
 
-# keywords : hash tag that you want to search  for like , retweet, follow
+results_search=5 # Number of Results for any keyword search for apply action.
+blacklisted = ["sex","sexy","porn"] # Keywords that you want to ignore
+
+# keywords : Hashtags that you want to search  for like
 keywords = ["microsoft", "itsm", "itam","business","monitoring","datascience","entrepreneur","startup"]
-messages = []
